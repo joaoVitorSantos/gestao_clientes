abf29db6cd2386562fe414329802657d5e0f39b0
joaoVitorSantos <joaovitorjec@gmail.com>
2019-06-27 15:25:46 -0300

    aula 12

--- gestao_clientes/clientes/urls.py
+++ gestao_clientes/clientes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import persons_list
+from .views import persons_list, PersonCreate, PersonUpdate
 from .views import persons_new
 from .views import persons_update
 from .views import persons_delete
 from .views import PersonList
+from .views import PersonDetail
 
 
 urlpatterns = [
@@ -11,5 +12,8 @@ urlpatterns = [
     path('new/', persons_new, name="person_new"),
     path('update/<int:id>/', persons_update, name="persons_update"),
     path('delete/<int:id>/', persons_delete, name="persons_delete"),
-    path('person_list/', PersonList.as_view())
+    path('person_list/', PersonList.as_view(), name="person_list_cbv"),
+    path('person_detail/<int:pk>/', PersonDetail.as_view(), name="person_detail_cbv"),
+    path('person_create/', PersonCreate.as_view(), name="person_create_cbv"),
+    path('person_update/<int:pk>/', PersonUpdate.as_view(), name="person_update_cbv"),
 ]
